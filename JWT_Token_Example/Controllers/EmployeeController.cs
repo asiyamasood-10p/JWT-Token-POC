1efa38a97f47d15251ba641a7b32aef7478772cf
asiya.masood <asiya.masood@ASIYA-MASOOD>
2024-12-19 17:43:56 +0500

    JWT token implementation

---
+++ JWT_Token_Example/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using JWT_Token_Example.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Token_Example.Controllers
+{
+    [Route("api/Employee")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string AddUsers(Users user)
+        {
+            return "User added with username " + user.Username;
+        }
+    }
+}
---
+++ JWT_Token_Example/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using JWT_Token_Example.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JWT_Token_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+            IActionResult response = Unauthorized();
+            var loginUser = AuthenticateUser(user);
+            if (loginUser != null)
+            {
+                var token = GenerateToken(loginUser);
+                response = Ok(new { token = token });
+            }
+            return response;
+        }
+
+        private Users AuthenticateUser(Users user)
+        {
+            Users _user = null;
+            if (user.Username == "admin" && user.Password == "12345")
+            {
+                _user = new Users { Username = "Asiya Masood" };
+            }
+            return _user;
+        }
+
+        private string GenerateToken(Users user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"], null, DateTime.Now.AddMinutes(1), signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token); //it will create and return the token
+        }
+    }
+}
